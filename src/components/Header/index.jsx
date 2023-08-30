fd3bb5231dca03ea1698b85a22611746f149ac14
Davranbek <davranbek_1995@mail.ru>
2023-08-30 22:54:18 +0500

    added style to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import {Button} from "flowbite-react";
 import "./style.scss";
 
@@ -9,8 +9,8 @@ const index = () => {
         <header>
             <div className="container">
                 <nav className="h-[80px] bg-teal-200 flex items-center justify-between px-2">
-                    <a href="">LOGO</a>
-                    <ul className="flex items-center gap-x-20">
+                    <Link to="/" className="font-bold text-[25px]">LOGO</Link>
+                    <ul className="flex items-center gap-x-20 tetx-[18px] font-semibold">
                         <li><NavLink to="/">Home</NavLink></li>
                         <li><NavLink to="/about">About</NavLink></li>
                         <li><NavLink to="/products">Products</NavLink></li>
